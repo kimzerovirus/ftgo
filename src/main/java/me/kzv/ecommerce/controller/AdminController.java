90a0c7be7cc8167608bf86aee6f5a6fc2a815b28
kimzerovirus <yg9699k@naver.com>
2022-12-23 12:23:23 +0900

    feat: 관리자 대시보드 ui레이아웃 완성

--- src/main/java/me/kzv/ecommerce/controller/AdminController.java
+++ src/main/java/me/kzv/ecommerce/controller/AdminController.java
@@ -1,4 +1,13 @@
 package me.kzv.ecommerce.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminHome(){
+        return "admin/index";
+    }
 }
